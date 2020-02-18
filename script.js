731d61a36531a8b1c3f1bb459f69768d5843e5b6
Cecilia Castillo <cecilia.castillo@focussi.com>
2020-02-18 13:32:13 -0700

    Deep Dive Coding - prework 2 - shopping list part 2.

--- script.js
+++ script.js
@@ -11,22 +11,46 @@ function initShoppingList(){
 
 function handleItemForForm(event, formRef){
 	//backward compatibility with older browsers
-	if (event.preventDefault()) {
+	if (event.preventDefault) {
 		event.preventDefault();
 	}
 
 	addItemToShoppingList();
+	formRef.reset();
+
 	return false;
+}
+
+function handleDeleteItem(elementId){
+	let element = document.getElementById("button"+elementId);
+	element.addEventListener("click", () =>{
+		deleteListItem(elementId);
+	})
 }
 
 function addItemToShoppingList(){
 	let itemName = document.getElementById("item-name");
 	let itemAmount = document.getElementById("item-amount");
-	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+	let id = createRandomInt(1,999);
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
 	let itemListRef = document.getElementById("shopping-list");
 	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+	handleDeleteItem(id);
 }
 
-function createListItemHtml(itemName, itemAmount){
-	return `<li> ${itemName} - ${itemAmount}</li>`;
+function deleteListItem(elementId){
+	let item = document.getElementById("item"+elementId);
+	item.parentNode.removeChild(item);
+}
+
+function createListItemHtml(itemName, itemAmount, elementId){
+	return `<li id="item${elementId}"> ${itemName} - ${itemAmount}
+				<button type="button" id="button${elementId}">Delete Item</button>
+				</li>`;
+}
+
+function createRandomInt(min, max){
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
